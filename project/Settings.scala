ac4649029d686d4618733b01d7d8fd8dc016119e
unmesh <unmeshjoshi@gmail.com>
2017-10-25 07:36:14 +0530

    Scala version 2.12.4

--- project/Settings.scala
+++ project/Settings.scala
@@ -18,9 +18,8 @@ object Settings {
 
   lazy val defaultSettings = buildSettings ++ Seq(
     // compile options ScalaUnidoc, unidoc
-    scalacOptions ++= Seq("-target:jvm-1.8", "-encoding", "UTF-8", "-feature", "-deprecation", "-unchecked"),
-    javacOptions in Compile ++= Seq("-source", "1.8"),
-    javacOptions in(Compile, compile) ++= Seq("-add-modules","java.xml.bind","-source", "1.8", "-target", "1.8", "-Xlint:unchecked", "-Xlint:deprecation"),
+    scalacOptions ++= Seq("-encoding", "UTF-8", "-feature", "-deprecation", "-unchecked"),
+    javacOptions in(Compile, compile) ++= Seq("--add-modules","java.xml.bind","-source", "9", "-target", "9", "-Xlint:unchecked", "-Xlint:deprecation"),
     javaOptions in(Test, run) ++= Seq("-Djava.net.preferIPv4Stack=true") // For location service use
 
   )
