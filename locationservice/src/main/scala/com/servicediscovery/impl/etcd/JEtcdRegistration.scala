14dfc3fcd4597f38f3a69dfd539b7ae29c4661a8
unmesh <unmeshjoshi@gmail.com>
2017-10-25 06:00:25 +0530

    Graphstage for etcd events

--- locationservice/src/main/scala/com/servicediscovery/impl/etcd/JEtcdRegistration.scala
+++ locationservice/src/main/scala/com/servicediscovery/impl/etcd/JEtcdRegistration.scala
@@ -1,9 +1,12 @@
 package com.servicediscovery.impl.etcd
 
 import java.net.InetAddress
+import java.util
 
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.Source
 import akka.stream.stage.{GraphStage, GraphStageLogic, OutHandler}
-import akka.stream.{Attributes, Outlet, SourceShape}
+import akka.stream.{ActorMaterializer, Attributes, Outlet, SourceShape}
 import com.coreos.jetcd.Client
 import com.coreos.jetcd.data.ByteSequence
 import com.coreos.jetcd.watch.{WatchEvent, WatchResponse}
@@ -13,32 +16,58 @@ import scala.collection.immutable.Queue
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class EtcdWatchingGraphStage extends GraphStage[SourceShape[String]] {
-  override def createLogic(inheritedAttributes: Attributes) = new GraphStageLogic(shape) with OutHandler {
-    var buffer = Queue[WatchEvent]()
-    override def onPull(): Unit = {
-      if (!buffer.isEmpty) {
-        push(out, buffer.dequeue)
-      }
-      val callback = getAsyncCallback(connected)
-      val etcdFuture = Future {
-        val response: WatchResponse = ???
-        response
-      }
-      etcdFuture.foreach(response ⇒ connected(response))
-    }
+class EtcdWatchingGraphStage extends GraphStage[SourceShape[WatchEvent]] {
+  val out = Outlet[WatchEvent]("etcd.out")
 
-    private def connected(watchResponse: WatchResponse): Unit = {
-      import scala.collection.JavaConverters._
-      val events = watchResponse.getEvents
-      buffer = buffer ++ events.asScala
-    }
-  }
-
-  val out = Outlet[String]("etcd.out")
   override def shape = SourceShape.of(out)
 
+  override def createLogic(inheritedAttributes: Attributes) = new GraphStageLogic(shape) with OutHandler {
+    var buffer = Queue[WatchEvent]() //TODO: Possibly use Source.queue
+    override def onPull(): Unit = {
+      if (!buffer.isEmpty) {
+        pushIfReady
+      } else {
+        val callback = getAsyncCallback(handleEtcdResponse)
+        val etcdFuture = new EtcdService().watch("/skydns/local/skydns/service/account/1")
+        etcdFuture.foreach(response ⇒ callback.invoke(response))
 
+      }
+    }
+
+    private def pushIfReady = {
+      if (isAvailable(out)) {
+        val value: (WatchEvent, Queue[WatchEvent]) = buffer.dequeue
+        buffer = value._2
+        println(s"Output port available. Pushing value ${value._1}")
+        push(out, value._1)
+      }
+    }
+
+    private def handleEtcdResponse(events: util.List[WatchEvent]): Unit = {
+      import scala.collection.JavaConverters._
+      println(s"Adding BUFFER ${events}")
+      buffer = buffer ++ events.asScala
+      pushIfReady
+    }
+
+    setHandler(out, this)
+  }
+}
+
+class EtcdService {
+  val hostIp: InetAddress = new Networks().ipv4Address //get ip address of primary interface.
+  def url(hostIp: InetAddress, port: Int) = s"http:/${hostIp}:${port}"
+
+  val etcdClient = Client.builder.endpoints(url(hostIp, 4002), url(hostIp, 4001)).build
+
+  def watch(key: String): Future[util.List[WatchEvent]] = Future {
+    val watchClient = etcdClient.getWatchClient
+    import com.coreos.jetcd.data.ByteSequence
+    val watcher = watchClient.watch(ByteSequence.fromString(key))
+    println("waiting for value")
+    val response: WatchResponse = watcher.listen()
+    response.getEvents
+  }
 }
 
 object JEtcdRegistration extends App {
@@ -49,28 +78,18 @@ class JEtcdRegistration {
 
 
   val hostIp: InetAddress = new Networks().ipv4Address //get ip address of primary interface.
-  def url(hostIp:InetAddress, port:Int) = s"http:/${hostIp}:${port}"
+  def url(hostIp: InetAddress, port: Int) = s"http:/${hostIp}:${port}"
+
   println(s"host ip address is ${hostIp}")
+  implicit val actorSystem = ActorSystem("etcdSystem")
+  implicit val actorMaterializer = ActorMaterializer()
 
   def register = {
+    //
+    //    val eventualEvents = new EtcdService().watch("/skydns/local/skydns/service/account/1")
+    //    eventualEvents.foreach(event⇒println(event))
 
-    val eventualUnit1 = new Thread(() ⇒ {
-      val etcdClient = Client.builder.endpoints(url(hostIp, 4002), url(hostIp, 4001)).build
-
-      while (true) {
-        val watchClient = etcdClient.getWatchClient
-        import com.coreos.jetcd.data.ByteSequence
-        val watcher = watchClient.watch(ByteSequence.fromString("/skydns/local/skydns/service/account/1"))
-        println("waiting for value")
-        val response: WatchResponse = watcher.listen()
-        println(s"got response ${response}")
-        response.getEvents.forEach(action ⇒ {
-          println(new String(action.getKeyValue.getValue.getBytes))
-        })
-      }
-    }
-
-    ).start()
+    Source.fromGraph(new EtcdWatchingGraphStage).runForeach(watchEvent ⇒ println(s"---------------------------------------------------------------------------------${watchEvent.getEventType}:${watchEvent.getKeyValue.getKey}=${watchEvent.getKeyValue.getValue}"))
 
     var i = 0;
     val etcdClient = Client.builder.endpoints(url(hostIp, 4002), url(hostIp, 4001)).build
